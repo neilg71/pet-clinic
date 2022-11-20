1e0758c68153ab71649751bacff989b07299a990
Neil Goldsmith <neilgoldsmith1218@gmail.com>
2022-11-19 23:18:07 -0500

    refactored base interface for services closes #11

--- petclinic-data/src/main/java/com/adw/petclinic/petclinic/services/OwnerService.java
+++ petclinic-data/src/main/java/com/adw/petclinic/petclinic/services/OwnerService.java
@@ -2,12 +2,7 @@ package com.adw.petclinic.petclinic.services;
 
 import com.adw.petclinic.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLasName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- petclinic-data/src/main/java/com/adw/petclinic/petclinic/services/VetService.java
+++ petclinic-data/src/main/java/com/adw/petclinic/petclinic/services/VetService.java
@@ -2,11 +2,6 @@ package com.adw.petclinic.petclinic.services;
 
 import com.adw.petclinic.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
